210d55612a3c5e071de567c8448a3ff2e7a6c479
Brian Hulette <hulettbh@gmail.com>
2014-08-24 00:42:37 -0400

    now generates two signals at different baud rates, makes a nice plot of cyclic spectrum at different alphas

--- simple_tx.m
+++ simple_tx.m
@@ -1,12 +1,13 @@
-function [ out_samples ] = simple_tx( input_bits )
-%% Load the config file to adjust parameters %%
-configuration;
+function [ out_samples ] = simple_tx( input_bits, modulation, samps_per_sym, F_S )
+RC_ROLLOFF = 0.25;          % Adjusts alpha of the RRC pulse shape
 
 %% Convert the encoded bits to symbols (at complex baseband) %%
-modulated_samples = modulate(input_bits, MODULATION, SAMPLES_PER_SYMBOL);
+modulated_samples = modulate(input_bits, modulation, samps_per_sym);
 
 %% Apply the pulse shaping %%
-out_samples = apply_filt(modulated_samples, PULSE_SHAPE);
+T = samps_per_sym/F_S; % symbol time
+pulse_shape = generate_pulse_shaping_filt(F_S, T, RC_ROLLOFF);
+out_samples = apply_filt(modulated_samples, pulse_shape);
 
 end
 
--- test_cyclo.m
+++ test_cyclo.m
@@ -3,7 +3,7 @@ configuration;
 
 numbits = 2048*100;
 input_bits = randn(1, numbits)<0;
-EBNO = 50;
+EBNO = 20;
 
 UP = 4;
 
@@ -15,36 +15,64 @@ noise_variance = 1./(2.*EsNo); % Convert Es_No to variance
 
 %% Run the Transmitter
 %tx = MyTransmitter(input_bits);
-tx = simple_tx(input_bits);
+% Generate channel 1 at baud rate fs/4
+chan1 = simple_tx(input_bits, 'QPSK', 4, F_S);
+% Generate channel 2 at baud rate fs/8
+chan2 = simple_tx(input_bits(1:length(input_bits)/2), 'QPSK', 8, F_S);
  
+% upsample data
+Hd=design(fdesign.lowpass('Fp,Fst',3/4/UP, 1/UP), 'equiripple');
+
+chan1 = upsample(chan1, UP);
+chan1 = filter(Hd, chan1);
+chan2 = upsample(chan2, UP);
+chan2 = filter(Hd, chan2);
+
+% shift frequency up, combine both channels
+t = 0:1/F_S/UP:(length(chan1) - 1)/F_S/UP;
+tx = chan1.*exp(2*pi*j*F_S*t) + chan2.*exp(-1*pi*j*2*F_S*t);
+
 tx = awgnChannel(tx, noise_variance, F_S, 0, 0, 0, 0);
 
-% upsample data
-tx = upsample(tx, UP);
-Hd=design(fdesign.lowpass('Fp,Fst',3/4/UP, 1/UP), 'equiripple');
-tx = filter(Hd, tx);
-
-% shift frequency up
-t = 0:1/F_S/UP:(length(tx) - 1)/F_S/UP;
-tx = tx.*exp(2*pi*j*F_S*t);
 
 plot_spectrum(tx, F_S*UP);
 
-nfft = 1024;
+nfft = 256;
 
-plot_cyc_spec(tx, 0,     nfft, F_S*UP);
-plot_cyc_spec(tx, 1000,  nfft, F_S*UP);
-plot_cyc_spec(tx, F_S/2, nfft, F_S*UP);
-plot_cyc_spec(tx, F_S/4, nfft, F_S*UP);
-plot_cyc_spec(tx, F_S/8, nfft, F_S*UP);
-plot_cyc_spec(tx, 4E6,   nfft, F_S*UP);
+alpha1 = 0;
+alpha2 = F_S/8;
+alpha3 = F_S/4;
+alpha4 = 3*F_S/8;
+alpha5 = F_S/2;
 
-spec_size = 100;
-cyc_spec = zeros(spec_size, nfft);
-for idx = 1:spec_size
-    alpha = (idx-1)/spec_size*(F_S); % scale alpha from 0 to F_S/2
-    cyc_spec(idx,:) = cyclic_spectrum(tx, alpha, nfft, F_S*UP);
-end
+cyc1 = cyclic_spectrum(tx, alpha1, nfft, F_S*UP) ;
+cyc2 = cyclic_spectrum(tx, alpha2, nfft, F_S*UP) ;
+cyc3 = cyclic_spectrum(tx, alpha3, nfft, F_S*UP) ;
+cyc4 = cyclic_spectrum(tx, alpha4, nfft, F_S*UP) ;
+cyc5 = cyclic_spectrum(tx, alpha5, nfft, F_S*UP) ;
 
-%surf(10*log(abs(cyc_spec)));
-surf(abs(cyc_spec));
+f = linspace(-F_S/2, F_S/2, nfft);
+
+figure;
+plot3(f, alpha1*ones(1,nfft), abs(cyc1), '-b', ...
+      f, alpha2*ones(1,nfft), abs(cyc2), '-b', ...
+      f, alpha3*ones(1,nfft), abs(cyc3), '-b', ...
+      f, alpha4*ones(1,nfft), abs(cyc4), '-b', ...
+      f, alpha5*ones(1,nfft), abs(cyc5), '-b');
+    
+%plot_cyc_spec(tx, 0,     nfft, F_S*UP);
+%plot_cyc_spec(tx, 1000,  nfft, F_S*UP);
+%plot_cyc_spec(tx, F_S/2, nfft, F_S*UP);
+%plot_cyc_spec(tx, F_S/4, nfft, F_S*UP);
+%plot_cyc_spec(tx, F_S/8, nfft, F_S*UP);
+%plot_cyc_spec(tx, 4E6,   nfft, F_S*UP);
+
+%spec_size = 100;
+%cyc_spec = zeros(spec_size, nfft);
+%for idx = 1:spec_size
+%    alpha = (idx-1)/spec_size*(F_S); % scale alpha from 0 to F_S/2
+%    cyc_spec(idx,:) = cyclic_spectrum(tx, alpha, nfft, F_S*UP);
+%end
+%
+%%surf(10*log(abs(cyc_spec)));
+%surf(abs(cyc_spec));
