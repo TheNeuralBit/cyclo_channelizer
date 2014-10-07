8ff91dcb819decc64d654946bb57fea7f58d7d34
Brian Hulette <hulettbh@gmail.com>
2014-10-07 19:46:33 -0400

    fiddled with signal rates and bauds calculated

--- test_cyclo.m
+++ test_cyclo.m
@@ -16,7 +16,7 @@ noise_variance = 1./(2.*EsNo); % Convert Es_No to variance
 %% Run the Transmitter
 %tx = MyTransmitter(input_bits);
 % Generate channel 1 at baud rate fs/4
-chan1 = simple_tx(input_bits, 'QPSK', 4, F_S);
+chan1 = simple_tx(input_bits(1:length(input_bits)/4), 'QPSK', 16, F_S);
 % Generate channel 2 at baud rate fs/8
 chan2 = simple_tx(input_bits(1:length(input_bits)/2), 'QPSK', 8, F_S);
  
@@ -44,21 +44,26 @@ alpha2 = F_S/8;
 alpha3 = F_S/4;
 alpha4 = 3*F_S/8;
 alpha5 = F_S/2;
+alpha6 = F_S/16;
 
 cyc1 = cyclic_spectrum(tx, alpha1, nfft, F_S*UP) ;
 cyc2 = cyclic_spectrum(tx, alpha2, nfft, F_S*UP) ;
 cyc3 = cyclic_spectrum(tx, alpha3, nfft, F_S*UP) ;
 cyc4 = cyclic_spectrum(tx, alpha4, nfft, F_S*UP) ;
 cyc5 = cyclic_spectrum(tx, alpha5, nfft, F_S*UP) ;
+cyc6 = cyclic_spectrum(tx, alpha6, nfft, F_S*UP) ;
 
 f = linspace(-F_S/2, F_S/2, nfft);
 
 figure;
+h1 = axes;
 plot3(f, alpha1*ones(1,nfft), abs(cyc1), '-b', ...
       f, alpha2*ones(1,nfft), abs(cyc2), '-b', ...
       f, alpha3*ones(1,nfft), abs(cyc3), '-b', ...
       f, alpha4*ones(1,nfft), abs(cyc4), '-b', ...
-      f, alpha5*ones(1,nfft), abs(cyc5), '-b');
+      f, alpha5*ones(1,nfft), abs(cyc5), '-b', ...
+      f, alpha6*ones(1,nfft), abs(cyc6), '-b');
+set(h1, 'Ydir', 'reverse');
     
 %plot_cyc_spec(tx, 0,     nfft, F_S*UP);
 %plot_cyc_spec(tx, 1000,  nfft, F_S*UP);
