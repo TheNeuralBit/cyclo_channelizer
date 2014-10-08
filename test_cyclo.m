e23ab903f1aea31414fa2557ca31263c0cc44391
Brian Hulette <hulettbh@gmail.com>
2014-10-07 21:15:46 -0400

    Adjusted power calculation

--- test_cyclo.m
+++ test_cyclo.m
@@ -3,15 +3,12 @@ configuration;
 
 numbits = 2048*100;
 input_bits = randn(1, numbits)<0;
-EBNO = 20;
-
+PN = 20;
 UP = 4;
 
-%% Other Parameters
-SAMPLES_PER_SYMBOL = 4;  % given, samp/symbol >= 4
-EsNo = CODE_RATE.*BITS_PER_SYMBOL.*10.^(0.1.*EBNO);
-% EsNo = CODE_RATE.*BITS_PER_SYMBOL.*10.^(0.1.*EBNO);
-noise_variance = 1./(2.*EsNo); % Convert Es_No to variance
+% Compute Noise variance based on desired noise power
+% pretty sure this is wrong
+noise_variance = 10.^(0.1*PN)/(F_S*UP*2);
 
 %% Run the Transmitter
 %tx = MyTransmitter(input_bits);
@@ -23,21 +20,21 @@ chan2 = simple_tx(input_bits(1:length(input_bits)/2), 'QPSK', 8, F_S, .25);
 % upsample data
 Hd=design(fdesign.lowpass('Fp,Fst',3/4/UP, 1/UP), 'equiripple');
 
-chan1 = upsample(chan1, UP);
-chan1 = filter(Hd, chan1);
-chan2 = upsample(chan2, UP);
-chan2 = filter(Hd, chan2);
+chan1_up = upsample(chan1, UP);
+chan1_up = filter(Hd, chan1_up);
+chan2_up = upsample(chan2, UP);
+chan2_up = filter(Hd, chan2_up);
 
 % shift frequency up, combine both channels
-t = 0:1/F_S/UP:(length(chan1) - 1)/F_S/UP;
-tx = chan1.*exp(2*pi*j*F_S*t) + chan2.*exp(-1*pi*j*2*F_S*t);
+t = 0:1/F_S/UP:(length(chan1_up) - 1)/F_S/UP;
+tx = chan1_up.*exp(2*pi*j*F_S*t) + chan2_up.*exp(-1*pi*j*2*F_S*t);
 
-tx = awgnChannel(tx, noise_variance, F_S, 0, 0, 0, 0);
+tx = awgnChannel(tx, noise_variance, F_S*UP, 0, 0, 0, 0);
 
 
 plot_spectrum(tx, F_S*UP);
 
-nfft = 256;
+nfft = 1024;
 
 alpha1 = 0;
 alpha2 = F_S/8;
@@ -53,7 +50,7 @@ cyc4 = cyclic_spectrum(tx, alpha4, nfft, F_S*UP) ;
 cyc5 = cyclic_spectrum(tx, alpha5, nfft, F_S*UP) ;
 cyc6 = cyclic_spectrum(tx, alpha6, nfft, F_S*UP) ;
 
-f = linspace(-F_S/2, F_S/2, nfft);
+f = linspace(-F_S*UP/2, F_S*UP/2, nfft);
 
 figure;
 h1 = axes;
