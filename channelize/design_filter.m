86fa81aa26bdb1488ca1add6473cd083745fba0a
Brian Hulette <hulettbh@gmail.com>
2015-01-02 16:08:41 -0500

    now caches filters once they have been generated

--- channelize/design_filter.m
+++ channelize/design_filter.m
@@ -1,5 +1,11 @@
 function b = design_filter(f_s, num_channels)
-cutoff = f_s/num_channels/2;
+filename = sprintf('filt_%d.mat', num_channels);
+if exist(filename)
+    load(filename, 'b');
+    return
+end
+
+cutoff = 1/num_channels/2;
 rp = 3;           % Passband ripple
 rs = 50;          % Stopband ripple
 f = [0.8*cutoff 1.2*cutoff];    % Cutoff frequencies
@@ -8,13 +14,15 @@ a = [1 0];        % Desired amplitudes
 % Compute deviations
 dev = [(10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)]; 
 
-c = firpmord( f, a, dev, f_s, 'cell');
+c = firpmord( f, a, dev, 1, 'cell');
 b = firpm(c{:});
 
 Hd=design(fdesign.lowpass('Fp,Fst',0.8/num_channels, 1.2/num_channels), 'equiripple');
 
 s = coeffs(Hd);
 b = s.Numerator;
+
+save(filename, 'b');
 
 %b=remez(511,[0 96 160 192*32]/(192*32),[1 1 0 0],[1 13]);
 %b(1)=b(2)/4;
