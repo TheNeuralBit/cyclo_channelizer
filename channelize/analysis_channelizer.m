06ea1d5bbb799692c28653e671ccf99bbeb0702c
Brian Hulette <hulettbh@vt.edu>
2015-03-05 16:44:09 -0500

    started implementation of analysis NMDFB - not working yet

--- channelize/analysis_channelizer.m
+++ channelize/analysis_channelizer.m
@@ -1,42 +1,54 @@
 function [channels] = analysis_channelizer(data, num_channels)
-% analysis_channelizer - Split a wideband signal into D channels each samples
-%                        at fs/D
+% analysis_channelizer - Split a wideband signal into M channels each sampled
+%                        at fs/D, D=M/2
 % Input:  data         - 1D vector of time domain data. Wideband signal to
 %                        be filtered.
-%         num_channels - Number of channels to produce, D
+%         num_channels - Number of channels to produce, M
 %
 % Output: channels     - 1xD cell array of time domain data for each channel.
 %                        item at index k has center frequency -fs/2 + kfs/D
     FFT_SIZE = num_channels;
     
+    decimation = num_channels/2;
     channels = cell(num_channels, 1);
+    M = num_channels;
+    D = decimation;
     
     %% DESIGN THE FILTER
     disp('Designing the filter...')
     b = design_filter(num_channels);
-    
+   
     fvtool(b, 1);
     
     % zero pad
-    b = [b zeros(1, FFT_SIZE - (mod(length(b) - 1, FFT_SIZE) + 1))];
-    b = reshape(b, FFT_SIZE, []);
+    b = [b zeros(1, 2*FFT_SIZE - (mod(length(b) - 1, 2*FFT_SIZE) + 1))];
+    b = reshape(b, FFT_SIZE, [])
+    b_upper = b(1:M/2,1:2:end)
+    b_lower = b(M/2+1:end,2:2:end)
     
     %% FILTER THE DATA
     disp('Performing polyphase filtering...')
     %zero pad
     data = [data zeros(1, FFT_SIZE - (mod(length(data) - 1, FFT_SIZE) + 1))];
     % put data into a matrix by rows, so we can compute filters across each row
-    data_2d = flipud(reshape(data, FFT_SIZE, []));
+    data_2d = flipud(reshape(data, M/2, []));
     
-    filt_output = zeros(size(data_2d));
-    for i=1:FFT_SIZE
-        filt_output(i, :) = filter(b(i,:), 1, data_2d(i, :));
+    filt_output = zeros(M, length(data)/D);
+    for i=1:M/2
+        filt_output(i, :) = filter(b_upper(i,:), 1, data_2d(i, :));
+    end
+    for i=1:M/2
+        filt_output(i + M/2, :) = filter(b_lower(i,:), 1, data_2d(i, :));
+    end
+
+    for j=2:2:size(filt_output, 2)
+        filt_output(:, j) = circshift(filt_output(:, j), M/2);
     end
     
     %% FFT
     disp('Performing FFT...')
     % compute fft of each column
-    channels = flipud(fftshift(fft(filt_output, FFT_SIZE, 1), 1));
+    channels = flipud(fftshift(ifft(filt_output, FFT_SIZE, 1), 1));
     
     channels = num2cell(channels, 2);
 end
