c5c12b0605414c297f6df50144140335cafee311
Brian Hulette <hulettbh@vt.edu>
2015-04-02 20:18:34 -0400

    got rid of extraneous plot in plot_results, fixed axis labels

--- plot_results.m
+++ plot_results.m
@@ -16,15 +16,6 @@ function plot_results(glob)
         if idx > 1
             hold on;
         end
-        semilogy(num_signals, runtimes, colors(mod(idx - 1, length(colors)) + 1,:));
-        if idx > 1
-            hold off;
-        end
-        
-        figure(2);
-        if idx > 1
-            hold on;
-        end
         plot(num_signals, runtimes, colors(mod(idx - 1, length(colors)) + 1,:));
         if idx > 1
             hold off;
@@ -34,15 +25,8 @@ function plot_results(glob)
     figure(1);
     hold on;
     legend(legend_data);
-    title('BER')
-    xlabel('E_b/N_0 (dB)')
-    ylabel('BER')
+    title('Runtime')
+    xlabel('Number of Channels')
+    ylabel('Average Runtime (s)')
     hold off;
-
-    figure(2);
-    hold on;
-    legend(legend_data);
-    title('Throughput')
-    xlabel('E_b/N_0 (dB)')
-    ylabel('Throughput')
-    hold off;
+end
