29a21369d5b516a43beea9f1a67d5e57f7d0991f
Brian Hulette <hulettbh@vt.edu>
2015-04-12 12:04:18 -0400

    handled some edge cases that break things for weird sample rates

--- cyclo_and_polyphase.m
+++ cyclo_and_polyphase.m
@@ -15,9 +15,10 @@ function [channels output_f_s freqs] = cyclo_and_polyphase(data, bauds_to_check,
     min_spacing = CYCLO_PEAK_MIN_SPACING;
     fft_size = 1024*2;
 
-    potential_decimations = F_S./bauds_to_check./samps_per_sym;
     [freqs, bauds] = cyclo_detect(data, bauds_to_check, threshold, min_spacing, fft_size, F_S);
-    decimations = F_S./bauds./samps_per_sym;
+
+    % TODO: Warning if decimations not actually close to integer
+    decimations = round(F_S./bauds./samps_per_sym);
     channels = polyphase_channelizer(data, freqs, decimations, F_S);
     output_f_s = F_S./decimations;
 end
--- resample.m
+++ resample.m
@@ -1,6 +1,7 @@
 function [output_data] = resample(input_data, in_fs, out_fs)
     total_time = length(input_data)/in_fs;
     in_t  = 0:1/in_fs:total_time;
+    in_t = in_t(1:length(input_data));
     out_t = 0:1/out_fs:total_time;
-    output_data = interp1(in_t, input_data, out_t, 'cubic')
+    output_data = interp1(in_t, input_data, out_t, 'pchip');
 end
